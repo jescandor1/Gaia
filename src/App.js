ee11fe69042896c3ad59147684c29a07c8e2276b
jasmine <jescandor1@ufl.edu>
2025-02-08 00:59:16 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NewMotherChat from './components/NewMotherChat';
+
+function App() {
+  return <NewMotherChat />;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles/tailwind.css'; // Optional, if you're using Tailwind CSS
+
+ReactDOM.render(<App />, document.getElementById('root'));
